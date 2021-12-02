503b82be3f4e0a0f290b557d8449bd9c5fa6a130
AlDrean <lucas.henrique.waki@gmail.com>
2021-12-02 08:33:26 -0300

    Var types and println tests

--- project1/src/HelloWorld.kt
+++ project1/src/HelloWorld.kt
@@ -4,4 +4,22 @@ fun main() {
     var nome = "Lucas Henrique Waki"
     var idade = 27
     var cidade = "Campinas"
+
+
+    var numero: Int = 15
+    var pi:Float = 3.14f
+
+
+    println("Double: Max [" + Double.MAX_VALUE + "] - ["+Double.MIN_VALUE+"]")
+    println("int: Max [" + Int.MAX_VALUE + "] - ["+Int.MIN_VALUE+"]")
+    println("Long: Max [" + Long.MAX_VALUE + "] - ["+Long.MIN_VALUE+"]")
+    println("Short: Max [" + Short.MAX_VALUE + "] - ["+Short.MIN_VALUE+"]")
+    println("Float: Max [" + Float.MAX_VALUE + "] - ["+Float.MIN_VALUE+"]")
+
+
+    println("numero = $numero")
+    println("pi = $pi")
+    println("Cidade = $cidade")
+
+
 }
