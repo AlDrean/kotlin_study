a58f69cdb0ac269d6c742ae67e17a695486546dd
AlDrean <lucas.henrique.waki@gmail.com>
2021-12-02 22:18:33 -0300

    added forms and buttons; Can't open the forms correctly even with code given

--- Agenda/src/main/kotlin/UI/ContactForm.java
+++ Agenda/src/main/kotlin/UI/ContactForm.java
@@ -1,4 +1,50 @@
 package UI;
 
-public class ContactForm {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ContactForm  extends JFrame {
+    private JPanel rootPannel;
+    private JTextField txt_name;
+    private JTextField txt_fone;
+    private JButton saveButton;
+    private JButton cancelButton;
+
+    public ContactForm(){
+
+        setContentPane(rootPannel);
+        setSize(500, 250);
+        setVisible(true);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width  / 2 - getSize().width,
+                dim.height / 2 - getSize().height);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                new MainForm();
+                dispose();
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
--- Agenda/src/main/kotlin/UI/MainForm.java
+++ Agenda/src/main/kotlin/UI/MainForm.java
@@ -1,6 +1,9 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JPanel rootPannel;
@@ -8,11 +11,34 @@ public class MainForm extends JFrame {
     private JButton btn_remove;
     private JTable tbl_contacts;
 
-    public MainForm(){
+    public MainForm() {
 
         setContentPane(rootPannel);
-        setSize(500,250);
+        setSize(500, 250);
         setVisible(true);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width  / 2 - getSize().width,
+                    dim.height / 2 - getSize().height);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
     }
 
+    private void setListeners(){
+        btn_newContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                new ContactForm();
+                dispose();
+            }
+        });
+        btn_remove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
 }
